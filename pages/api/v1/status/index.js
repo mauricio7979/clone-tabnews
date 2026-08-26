feccf16fd27b44c7826f2a220af7b76ab15f4399
mauricio7979 <mauriciosousa7979@gmail.com>
2026-08-26 09:22:56 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("SELECT 1 + 1 as sum;")
+  console.log(result.rows[0])
   res.status(200).json({
     status: req.statusCode || 200,
     message: "Os alunos do Curso.dev são pessoas acima da média.",
